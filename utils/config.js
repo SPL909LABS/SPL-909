8089c0e85bf71506b0394587f8a90077165118de
whanum <eduardermakov769@gmail.com>
2025-12-05 22:20:13 +0800

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';      MELOAN
+import dotenv from 'dotenv';     
 import path from 'path';    
 
 dotenv.config({ path: path.resolve(process.cwd(), `.env.${process.env.NODE_ENV || 'development'}`) });
